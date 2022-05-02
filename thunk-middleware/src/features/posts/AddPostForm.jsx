f708e90353194eca818d7125f4c0f15d1a0fb711
MuhammadIkram24 <m.ikramch22@gmail.com>
2022-05-02 16:31:27 +0500

    added thunk-middleware

---
+++ thunk-middleware/src/features/posts/AddPostForm.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addNewPost } from "./posts.slice";
+import { selectAllUsers } from "../users/users.slice";
+
+const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
+
+  const dispatch = useDispatch();
+  const users = useSelector(selectAllUsers);
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onUserChanged = (e) => setUserId(e.target.value);
+
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
+
+  const savePost = (e) => {
+    e.preventDefault();
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (err) {
+        console.error("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
+    }
+  };
+
+  const usersOptions = users.map((user) => (
+    <option value={user.id} key={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <section>
+      <h2>Add a new post</h2>
+      <form onSubmit={savePost}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postUser">Author:</label>
+        <select
+          name="postUser"
+          id="postUser"
+          value={userId}
+          onChange={onUserChanged}
+        >
+          <option value="" disabled="disabled"></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="submit" disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default AddPostForm;
---
+++ thunk-middleware/src/features/posts/PostsList.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./posts.slice";
+
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
+
+  let content;
+  if (postsStatus === "loading") {
+    content = <p>'Loading...'</p>;
+  } else if (postsStatus === "fulfilled") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post, index) => (
+      <PostsExcerpt key={index} post={post} />
+    ));
+  } else if (postsStatus === "rejected") {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <section>
+      <h2>Posts List</h2>
+      {content}
+    </section>
+  );
+};
+
+export default PostsList;
